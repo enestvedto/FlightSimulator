d3fe85b8ffa378b943c6a484066a65c394e108f7
Toni <craveiroa@merrimack.edu>
2022-10-10 16:54:36 -0400

    Basic Setup
    Added a PointerLock and some grids

--- flightSimulation/main.js
+++ flightSimulation/main.js
@@ -1,8 +1,9 @@
 import './style.css'
 import * as THREE from 'three';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
 // Initalization of variables
-let scene, camera, renderer;
+let scene, camera, renderer, controls;
 let game_canvas = document.getElementById("myCanvas");
 let deltaTime;
 
@@ -33,15 +34,64 @@ function init() {
   camera = new THREE.PerspectiveCamera(45, game_canvas.clientWidth / game_canvas.clientHeight, 1, 2000);
   camera.name = 'camera';
   camera.position.z = 10;
+  camera.position.y = 2;
 
+  scene.add(camera);
+
+
+  // Geometry
+  let sphere = new THREE.SphereGeometry(1.5, 20, 16);
+
+  // Material
+  let materialOne = new THREE.MeshBasicMaterial({
+    color: 0xFF0000,
+  });
+
+  // Mesh
+  let mesh = new THREE.Mesh(sphere, materialOne);
+  scene.add(mesh);
 
   // Renderer
 
 
-  renderer = new THREE.WebGLRenderer({ canvas: game_canvas });
+  renderer = new THREE.WebGLRenderer( { canvas: game_canvas } );
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(game_canvas.clientWidth, game_canvas.clientHeight);
 
+  //GridHelper
+  let HelperXZ = new THREE.GridHelper( 100, 100 );
+  scene.add(HelperXZ);
+
+  let HelperXY = new THREE.GridHelper( 100, 100 );
+  HelperXY.rotation.x = Math.PI/2;
+  scene.add(HelperXY)
+
+  // Controls
+
+  let instructions = document.getElementById('instructions');
+  let blocker = document.getElementById('blocker');
+
+  controls = new PointerLockControls( camera, document.body );
+
+  instructions.addEventListener( 'click', function () {
+
+    controls.lock();
+
+  } );
+
+  controls.addEventListener( 'lock', function () {
+
+    instructions.style.display = 'none';
+    blocker.style.display = 'none';
+
+  } );
+
+  controls.addEventListener( 'unlock', function () {
+
+    blocker.style.display = 'block';
+    instructions.style.display = '';
+
+  } );
 
 }
 
